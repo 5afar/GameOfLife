ff251a85a48ae9ede9dc3158e039eb679dc2f93f
Danila <safardaf@yandex.ru>
2024-07-31 15:37:56 +0300

    math base complete

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,112 @@
 #include <SDL2/SDL.h>
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 
-constexpr int SCREEN_WIDTH = 640;
-constexpr int SCREEN_HEIGHT = 480;
-const char *WINDOW_TITLE = "Title";
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const int CELL_SIZE = 10;
+const int GRID_WIDTH = SCREEN_WIDTH / CELL_SIZE;
+const int GRID_HEIGHT = SCREEN_HEIGHT / CELL_SIZE;
 
-int main()
-{
-    SDL_Window *window = NULL;
-
-    SDL_Surface *screenSurface = NULL;
-
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        printf("SDL could not initialize! SDL_Errpr: %s\n", SDL_GetError());
-    }
-    else
-    {
-        window = SDL_CreateWindow(
-            WINDOW_TITLE,
-            SDL_WINDOWPOS_UNDEFINED,
-            SDL_WINDOWPOS_UNDEFINED,
-            SCREEN_WIDTH,
-            SCREEN_HEIGHT,
-            SDL_WINDOW_SHOWN);
-        if (window == NULL)
-        {
-            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+void initGrid(std::vector<std::vector<bool>>& grid) {
+    srand(time(0));
+    for (int i = 0; i < GRID_HEIGHT; ++i) {
+        for (int j = 0; j < GRID_WIDTH; ++j) {
+            grid[i][j] = rand() % 2;
         }
-        else
-        {
-            screenSurface = SDL_GetWindowSurface(window);
-            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-            SDL_UpdateWindowSurface(window);
-            SDL_Event e;
-            bool quit = false;
-            while (quit == false)
-            {
-                while (SDL_PollEvent(&e))
-                {
-                    if (e.type == SDL_QUIT)
-                        quit = true;
-                }
+    }
+}
+
+void renderGrid(SDL_Renderer* renderer, const std::vector<std::vector<bool>>& grid) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+    for (int i = 0; i < GRID_HEIGHT; ++i) {
+        for (int j = 0; j < GRID_WIDTH; ++j) {
+            if (grid[i][j]) {
+                SDL_Rect cell = {j * CELL_SIZE, i * CELL_SIZE, CELL_SIZE, CELL_SIZE};
+                SDL_RenderFillRect(renderer, &cell);
             }
-            SDL_DestroyWindow(window);
-            SDL_Quit();
         }
     }
+
+    SDL_RenderPresent(renderer);
+}
+
+int countLiveNeighbors(const std::vector<std::vector<bool>>& grid, int x, int y) {
+    int liveNeighbors = 0;
+    for (int i = -1; i <= 1; ++i) {
+        for (int j = -1; j <= 1; ++j) {
+            if (i == 0 && j == 0) continue;
+            int nx = x + i, ny = y + j;
+            if (nx >= 0 && nx < GRID_HEIGHT && ny >= 0 && ny < GRID_WIDTH) {
+                liveNeighbors += grid[nx][ny];
+            }
+        }
+    }
+    return liveNeighbors;
+}
+
+void updateGrid(std::vector<std::vector<bool>>& grid) {
+    std::vector<std::vector<bool>> newGrid = grid;
+    for (int i = 0; i < GRID_HEIGHT; ++i) {
+        for (int j = 0; j < GRID_WIDTH; ++j) {
+            int liveNeighbors = countLiveNeighbors(grid, i, j);
+            if (grid[i][j]) {
+                newGrid[i][j] = liveNeighbors == 2 || liveNeighbors == 3;
+            } else {
+                newGrid[i][j] = liveNeighbors == 3;
+            }
+        }
+    }
+    grid = newGrid;
+}
+
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Game of Life", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr) {
+        std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    std::vector<std::vector<bool>> grid(GRID_HEIGHT, std::vector<bool>(GRID_WIDTH, false));
+    initGrid(grid);
+
+    bool quit = false;
+    SDL_Event e;
+
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        updateGrid(grid);
+        renderGrid(renderer, grid);
+        SDL_Delay(100);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
